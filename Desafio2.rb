b799438c8ed8b073b5ad0358ae5bc8f0540afd41
Daniel Maximino <97485733+MaximinoDan@users.noreply.github.com>
2023-09-08 12:38:26 -0300

    Add files via upload

---
+++ Desafio2.rb
@@ -0,0 +1,20 @@
+num = []
+
+print ("Digite o primeiro número: ")
+num.push gets.chomp.to_i
+
+print ("Digite o segundo número: ")
+num.push gets.chomp.to_i
+
+print ("Digite o terceiro e último número: ")
+num.push gets.chomp.to_i
+
+potencia_numeros = num.map do |x|
+    x ** 3
+end
+
+puts "\n Números solicitados"
+puts "#{num}"
+
+puts"\n Números elevados a 3ª Potência"
+puts"#{potencia_numeros}"
